0565ca08f6ab4b9574a9c8dc75d3df888df4dca6
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-04-20 22:16:14 +0530

    modified

--- login.js
+++ login.js
@@ -6,40 +6,39 @@ function validate(){
     let passRegexp1 = /[A-Z]+/;
     let passRegexp2 = /[a-z]+/;
     let passRegexp3 = /[0-9]+/;
+    let entry;
+    let text= "<b>Error</b>";
 
     if (!EmailRegexp.test(email.value)){
-        alert("invalid email");
+        text += "<br>Enter a valid Email";
         email.style.border = "1.5px solid red";
-        return false;
-    } else if (pass.value.length < 8){
+        entry = false;
+    } if (pass.value.length < 8){
         pass.style.border = "1.5px solid red";
-        email.style.border = "0px solid ";
-        alert("Password must be atleast 8 charecters long.");
-        return false;
-    }else if (!passRegexp1.test(pass.value)){
+        text += "<br> Password must be atleast 8 charecters long.";
+        entry = false;
+    }if (!passRegexp1.test(pass.value)){
         pass.style.border = "1.5px solid red";
-        email.style.border = "0px solid ";
-        alert("Password must include a capital case letter");
-        return false;
-    }else if (!passRegexp2.test(pass.value)){
+        text += "<br>Password must include capital case letter";
+        entry = false;
+    }if (!passRegexp2.test(pass.value)){
         pass.style.border = "1.5px solid red";
-        email.style.border = "0px solid ";
-        alert("Password must include a small case letter");
-        return false;
-    }else if (!passRegexp3.test(pass.value)){
+        text += "<br>Password must include small case letter";
+        entry = false;
+    }if (!passRegexp3.test(pass.value)){
         pass.style.border = "1.5px solid red";
-        email.style.border = "0px solid ";
-        alert("Password must include a number");
-        return false;
-    }
-    else{
-        pass.style.border = "0px solid ";
+        text += "<br>Password must include number";
+        entry = false;
         
-        return true;
+    }if (entry==false){
+        document.getElementById("err").innerHTML= text;
+        document.getElementById("err").style.visibility = "visible";
+        return false;
     }
+    return true;
+    
+        
+        
+      
+    
 }
-
-
-
-
-
