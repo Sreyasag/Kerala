3f19049008599855f2bfb89f1ece10a1c8dbfaae
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-04-21 09:50:12 +0530

    signincolor

--- login.js
+++ login.js
@@ -11,22 +11,22 @@ function validate(){
 
     if (!EmailRegexp.test(email.value)){
         text += "<br>Enter a valid Email";
-        email.style.border = "1.5px solid red";
+        
         entry = false;
     } if (pass.value.length < 8){
-        pass.style.border = "1.5px solid red";
+        
         text += "<br> Password must be atleast 8 charecters long.";
         entry = false;
     }if (!passRegexp1.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+        
         text += "<br>Password must include capital case letter";
         entry = false;
     }if (!passRegexp2.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+        
         text += "<br>Password must include small case letter";
         entry = false;
     }if (!passRegexp3.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+        
         text += "<br>Password must include number";
         entry = false;
         
@@ -35,10 +35,35 @@ function validate(){
         document.getElementById("err").style.visibility = "visible";
         return false;
     }
-    return true;
+    return true; 
     
+}
+
+
+document.getElementById("Password1").addEventListener("keyup",showStrength)
+
+function showStrength(){
+    
+    let pass = document.getElementById("Password1");
+           
+    let passRegexp1 = /[A-Z]+/;
+    let passRegexp2 = /[a-z]+/;
+    let passRegexp3 = /[0-9]+/;
+    
+    let w= (pass.value.length >= 8);
+    let x= passRegexp1.test(pass.value);
+    let y= passRegexp2.test(pass.value);
+    let z= passRegexp3.test(pass.value);
+    let strength= Number(w)+Number(x)+Number(y)+Number(z);
         
         
-      
-    
+        if (strength<1){
+            pass.style.border = "2px solid rgb(248, 0, 0)";
+        } else if (strength<2){
+            pass.style.border = "2px solid rgb(255, 85, 0)";
+        }else if (strength<3){
+            pass.style.border = "2px solid rgb(255, 187, 0)";
+        }else if (strength>=4){
+            pass.style.border = "2px solid rgb(15, 201, 2)";
+        }
 }
