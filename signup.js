c8665478ec802c8b282e6b3f3f4e8fe0ff0cd4a6
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-04-21 09:45:22 +0530

    passcolor

--- signup.js
+++ signup.js
@@ -1,45 +1,51 @@
 
+
 function validate(){
-    let email = document.getElementById("email1");
-    let EmailRegexp = /^([\w\.\-]+)@(\w+).([a-z]{2,3})(.[a-z]{2,3})?$/;
-    let phone = document.getElementById("phone");
-    let phoneRegexp = /^\d{3}[\-\.\s]?\d{3}[\-\.\s]?\d{4}$/;
-    let pass = document.getElementById("Password1");
-    let passRegexp1 = /[A-Z]+/;
-    let passRegexp2 = /[a-z]+/;
-    let passRegexp3 = /[0-9]+/;
-    let pass2 = document.getElementById("Password2");
+let email = document.getElementById("email1");
+let phone = document.getElementById("phone");
+let pass = document.getElementById("Password1");
+let pass2 = document.getElementById("Password2");
+
+let EmailRegexp = /^([\w\.\-]+)@(\w+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+let phoneRegexp = /^\d{3}[\-\.\s]?\d{3}[\-\.\s]?\d{4}$/;
+let passRegexp1 = /[A-Z]+/;
+let passRegexp2 = /[a-z]+/;
+let passRegexp3 = /[0-9]+/;
+let a = (EmailRegexp.test(email.value));
+let b= phoneRegexp.test(phone.value);
+let c= (pass.value.length < 8);
+let d= passRegexp1.test(pass.value);
+let e= passRegexp2.test(pass.value);
+let f= passRegexp3.test(pass.value);
     let entry;
     let text= "<b>Error</b>";
-
-    if (!EmailRegexp.test(email.value)){
+    if (a==false){
         text += "<br>Enter a valid Email";
-        email.style.border = "1.5px solid red";
         entry = false;
-    } if (!phoneRegexp.test(phone.value)){
+    } if (b==false){
         text += "<br>Enter valid phone number";
-        phone.style.border = "1.5px solid red";
+        
         entry = false;
-    } if (pass.value.length < 8){
+    } if (c){
         text += "<br>Password must be atleast 8 characters long";
-        email.style.border = "1.5px solid red";
+        
         entry = false;
         
-    } if (!passRegexp1.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+    } if (!d){
+        
         text += "<br>Password must include capital case letter";
         entry = false;
         
-    } if (!passRegexp2.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+    } if (!e){
+        
         text += "<br>Password must include small case letter";
         entry = false;
-    } if (!passRegexp3.test(pass.value)){
-        pass.style.border = "1.5px solid red";
+    } if (!f){
+        
         text += "<br>Password must include number";
         entry = false;
     } if ((pass.value)!=(pass2.value)){
-        pass2.style.border = "1.5px solid red";
+        
         text += "<br>Re-entered password does not match";
         entry = false;
         
@@ -50,3 +56,33 @@ function validate(){
     }
     return true;
 }
+
+
+document.getElementById("Password1").addEventListener("keyup",showStrength)
+
+function showStrength(){
+    
+    let pass = document.getElementById("Password1");
+           
+    let passRegexp1 = /[A-Z]+/;
+    let passRegexp2 = /[a-z]+/;
+    let passRegexp3 = /[0-9]+/;
+    
+    let w= (pass.value.length >= 8);
+    let x= passRegexp1.test(pass.value);
+    let y= passRegexp2.test(pass.value);
+    let z= passRegexp3.test(pass.value);
+    let strength= Number(w)+Number(x)+Number(y)+Number(z);
+        
+        
+        if (strength<1){
+            pass.style.border = "2px solid rgb(248, 0, 0)";
+        } else if (strength<2){
+            pass.style.border = "2px solid rgb(255, 85, 0)";
+        }else if (strength<3){
+            pass.style.border = "2px solid rgb(255, 187, 0)";
+        }else if (strength>=4){
+            pass.style.border = "2px solid rgb(15, 201, 2)";
+        }
+}
+
